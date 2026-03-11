d590e41c0ce84d093f373886162086676bb30b71
Parker Pestana <parkerpestana@gmail.com>
2026-03-11 16:52:18 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Smooth scroll for nav links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
